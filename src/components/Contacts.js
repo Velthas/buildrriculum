1551c64049e13b68e4a67bac45fd8451f82c8d27
Velthas <damianobattaglia97@gmail.com>
2022-10-26 16:59:54 +0200

    Created contacts section & added relative styling

---
+++ src/components/Contacts.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import email from '../images/mail.svg';
+import web from '../images/web.svg';
+import smartphone from '../images/smartphone.svg';
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      edit: false,
+      buttons: false
+    }
+
+    let {changeInfo, displayButtons, hideButtons} = this.props;
+    this.changeInfo = changeInfo;
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.displayButtons = displayButtons.bind(this);
+    this.hideButtons = hideButtons.bind(this);
+  }
+
+  toggleEdit() {
+    this.setState({
+      edit: !this.state.edit,
+    });
+  }
+
+  render() {
+    let {telephone, mail, website} = this.props;
+
+    return (
+      <div className="cv-section flex-column centered-both contacts" onMouseEnter={this.displayButtons} onMouseLeave={this.hideButtons}>
+        { this.state.buttons && <button className="edit-button absolute-top-left" onClick={this.toggleEdit}>Modify</button> }
+        <h3 className="section-header">Contacts</h3>
+        <ul>
+          { telephone !== '' &&
+          <li className="flex-row">
+            <img src={ smartphone } alt="Mobile Icon"></img>
+            <p>{ telephone }</p>
+          </li>  }
+          { mail !== '' &&
+          <li className="flex-row">
+            <img src={ email } alt="Mail Icon"></img>
+            <p> { mail } </p>
+          </li> }
+          { website !== '' && 
+          <li className="flex-row">
+            <img src={ web } alt="Website Icon"></img>
+            <a href={ website }>{ website }</a>
+          </li> }
+        </ul>
+        { this.state.edit && 
+          <div className="form">
+
+            <form id="contacts-form">
+              <label for="contacts-tel">{'Telephone Number'}</label>
+              <input type="tel" placeholder="3334445555" id="contacts-tel"></input>
+
+              <label for="contacts-email">{'Email'}</label>
+              <input type="email" placeholder="example@provider.com" id="contacts-email"></input>
+
+              <label for="contacts-web">{'Website'}</label>
+              <input type="url" placeholder="https://eleanor.margarita.com/mix" id="contacts-web"></input>
+
+              <div className="flex-row">
+                <button class="button-cancel" type="button" onClick={this.toggleEdit}>Cancel</button>
+                <button class="button-submit" type="submit" onClick={(e) => {
+                  e.preventDefault();
+                  const info = Array.from(document.querySelectorAll('#contacts-form input')).map(input => input.value);
+                  const [ telInfo, mailInfo, siteInfo ] = info
+                  this.props.changeInfo('telephone', telInfo);
+                  this.props.changeInfo('mail', mailInfo);
+                  this.props.changeInfo('website', siteInfo);
+                  this.toggleEdit();
+                }}>Confirm</button>
+              </div>
+            </form>
+
+          </div> }
+    </div>
+    )
+  }
+}
+
+export default Contacts;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Eleanor from './../images/eleanor.jpeg';
 import Picture from './Picture';
 import Profile from './Profile';
+import Contacts from './Contacts';
 
 class Sidebar extends Component {
   constructor(props) {
@@ -10,11 +11,9 @@ class Sidebar extends Component {
     this.state = {
       photoUrl: Eleanor,
       profile: 'This is a description. You may wonder what it describes, or what even is its purpose in life, but truth is, I am not yet ready to disclose that information: in short, what should be a brief, informative paragraph, really has become a long, condescending, pedantic gathering of words, all cheering for the time they detracted from your total lifetime count. Please, dont hate me',
-      contacts: {
-        telephone: '202-555-01745',
-        mail: 'eleanor@goodplace.org',
-        website: 'https://otterhuggingastheysleep.goodplace.com',
-      },
+      telephone: '202-555-01745',
+      mail: 'eleanor@goodplace.org',
+      website: 'https://goodplace.com/Eleanor',
       languages: [['English', 'Fluent']]
     }
 
@@ -43,6 +42,7 @@ class Sidebar extends Component {
     return <div id="sidebar" className="flex-column centered">
       <Picture imgUrl={this.state.photoUrl} changeInfo={this.changeInfo} displayButtons={this.displayButtons} hideButtons={this.hideButtons} />
       <Profile profile={this.state.profile} changeInfo={this.changeInfo} displayButtons={this.displayButtons} hideButtons={this.hideButtons} />
+      <Contacts telephone={this.state.telephone} mail={this.state.mail} website={this.state.website} changeInfo={this.changeInfo} displayButtons={this.displayButtons} hideButtons={this.hideButtons} />
     </div>
   }
 }
